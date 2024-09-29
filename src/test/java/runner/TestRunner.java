10acad67a2e2ae1bfa18991d719872422510a885
s-sn-s <savin.sn.shetty@gmail.com>
2024-09-30 04:12:18 +0530

    InitCom

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,19 @@
+package runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(features = "src/test/resources/features",
+        glue = "org.cartOcart.stepDef",
+        plugin = {"pretty","html:target/cucumber-report","json:target/cucumber.json"},
+        monochrome = true,publish = true)
+public class TestRunner extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios(){
+        return super.scenarios();
+    }
+
+}
